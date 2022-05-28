a69a5269ad3883b89fee8402c3aaa73a11fb62e1
Anton Zavodchikov <terisbackno@gmail.com>
2022-05-28 19:20:58 +0500

    lil peek at Card component

---
+++ packages/app/src/lib/components/Card.svelte
@@ -0,0 +1,14 @@
+<div class="card">
+	<span class="value">4</span>
+	<slot />
+</div>
+
+<style>
+	.card {
+		position: relative;
+		width: 100px;
+		aspect-ratio: 64 / 89;
+		border-radius: 0.5em;
+		background-color: beige;
+	}
+</style>
--- packages/app/src/routes/index.svelte
+++ packages/app/src/routes/index.svelte
@@ -1,1 +1,7 @@
+<script>
+	import Card from "$lib/components/Card.svelte";
+</script>
+
 <p>There is should be a card game</p>
+
+<Card>Пэнис</Card>
