fb211235945ef4e49be823ffd6e76ffe398767b9
Anton Zavodchikov <terisbackno@gmail.com>
2022-05-31 11:34:46 +0500

    crop suits icons

--- packages/app/src/lib/assets/suits/Bugfix.svelte
+++ packages/app/src/lib/assets/suits/Bugfix.svelte
@@ -1,4 +1,21 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-	<path stroke="currentColor" stroke-linecap="round" stroke-width="1.5"
-		d="M9.64 8.5c1.154-2 4.041-2 5.196 0l4.041 7c1.155 2-.288 4.5-2.598 4.5H8.196c-2.31 0-3.753-2.5-2.598-4.5l4.042-7ZM12.5 11v4m0 2v.1" />
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="18"
+	height="18"
+	fill="none"
+	viewBox="0 0 18 18"
+>
+	<g clip-path="url(#a)">
+		<path
+			stroke="currentColor"
+			stroke-linecap="round"
+			stroke-width="1.5"
+			d="M6.64 5.5c1.154-2 4.041-2 5.196 0l4.041 7c1.155 2-.289 4.5-2.598 4.5H5.196c-2.31 0-3.753-2.5-2.598-4.5l4.042-7ZM9.5 8v4m0 2v.1"
+		/>
+	</g>
+	<defs>
+		<clipPath id="a">
+			<path fill="#fff" d="M0 0h18v18H0z" transform="matrix(-1 0 0 1 18 0)" />
+		</clipPath>
+	</defs>
 </svg>
--- packages/app/src/lib/assets/suits/Index.svelte
+++ packages/app/src/lib/assets/suits/Index.svelte
@@ -1,6 +1,19 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-	<path stroke="currentColor" stroke-width="1.5"
-		d="M17.482 6.412A3 3 0 0 0 14.541 4H9.459a3 3 0 0 0-2.941 2.412l-1.362 6.807a3 3 0 0 0 .096 1.537l1.064 3.193A3 3 0 0 0 9.162 20h5.676a3 3 0 0 0 2.846-2.051l1.064-3.193a3 3 0 0 0 .096-1.537l-1.362-6.807Z" />
-	<path stroke="currentColor" stroke-linecap="round" stroke-width="1.5"
-		d="M6 14h1.592a2 2 0 0 1 1.506.683l.554.634a2 2 0 0 0 1.506.683h1.684a2 2 0 0 0 1.506-.683l.554-.634A2 2 0 0 1 16.407 14H18M9.5 8h5m-6 3h7" />
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="18"
+	height="18"
+	fill="none"
+	viewBox="0 0 18 18"
+>
+	<path
+		stroke="currentColor"
+		stroke-width="1.5"
+		d="M14.482 3.412A3 3 0 0 0 11.541 1H6.459a3 3 0 0 0-2.941 2.412l-1.362 6.807a3 3 0 0 0 .096 1.537l1.064 3.193A3 3 0 0 0 6.162 17h5.676a3 3 0 0 0 2.846-2.051l1.064-3.193a3 3 0 0 0 .096-1.537l-1.362-6.807Z"
+	/>
+	<path
+		stroke="currentColor"
+		stroke-linecap="round"
+		stroke-width="1.5"
+		d="M3 11h1.592a2 2 0 0 1 1.506.683l.554.634A2 2 0 0 0 8.158 13h1.684a2 2 0 0 0 1.506-.683l.554-.634A2 2 0 0 1 13.408 11H15M6.5 5h5m-6 3h7"
+	/>
 </svg>
